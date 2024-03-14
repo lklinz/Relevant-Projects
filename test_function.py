d1dc8b2cc10a754416b8120a8699958b4fff4f81
Khanh Linh Le <142861969+lklinz@users.noreply.github.com>
2024-03-14 20:57:29 +0000

    recursion II

--- test_function.py
+++ test_function.py
@@ -1,15 +1,35 @@
-base_dict = {
-        1:1,
-        2:2,
-        3:4,
-        4:8
-    }
-def colored_blocks(n):
-    global base_dict 
-    if n in base_dict:
-        return base_dict[n]
-    else:
-        tiles_arranging = colored_blocks(n-1) + colored_blocks(n-2) + colored_blocks(n-3) + colored_blocks(n-4)
-        base_dict[n] = tiles_arranging
-        return tiles_arranging
-print(colored_blocks(50))
+def merge(array_one,array_two):
+    temporary_array = []
+    while len(array_one) > 0 and len(array_two) >0:
+        if array_one[0] > array_two[0]:
+            temporary_array.append(array_two[0])
+            array_two.remove(array_two[0])
+        else:
+            temporary_array.append(array_one[0])
+            array_one.remove(array_one[0])
+    while len(array_one) > 0: 
+        temporary_array.append(array_one[0])
+        array_one.remove(array_one[0])
+    while len(array_two) > 0:
+        temporary_array.append(array_two[0])
+        array_two.remove(array_two[0])  
+    return temporary_array
+
+def mergesort(array):
+    array_stored = []
+    if len(array) == 1:
+        return array
+    array1 = []
+    array2 = []
+    for num in range(len(array)):
+        if num < len(array)/2:
+            array1.append(array[num])
+        else:
+            array2.append(array[num])
+        array_stored.append(array2)
+    array1 = mergesort(array1)
+
+    print(merge(array1,array2))
+    while len(array_stored) > 0:
+        mergesort(array_stored)
+mergesort([6, 5, 5, 7, 4])
