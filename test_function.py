ac51ab6329f2f757a841fb0ab55f95c31365edca
Khanh Linh Le <142861969+lklinz@users.noreply.github.com>
2024-03-17 23:19:09 +0000

    finish recursively selection

--- test_function.py
+++ test_function.py
@@ -1,34 +1,14 @@
-def merge(array_one,array_two):
-
-    tempo_array = []
-    flag1 = flag2 = 0
-
-    while len(array_one) > flag1 and len(array_two) > flag2:
-        if array_one[flag1] > array_two[flag2]:
-            tempo_array.append(array_two[flag2])
-            flag2 += 1
-        else:
-            tempo_array.append(array_one[flag1])
-            flag1 += 1
-
-    tempo_array.extend(array_one[flag1:])
-    tempo_array.extend(array_two[flag2:])
-
-    return tempo_array
-
-
-def mergesort(array):
-
-    if len(array) == 1:
-        return array
-    mid = len(array) //2
-    array1 = array[:mid]
-    array2 = array[mid:]
-
-
-
-    array1 = mergesort(array1)
-    array2 = mergesort(array2)
-    return merge(array1,array2)
-
-mergesort([6, 5, 5, 7, 4, 8, 3, 5, 1, 2, 9])
+sorted_array = []
+def selection_recursive(array):
+    max_flag = array[0]
+    for num in array:
+        if num > max_flag:
+            max_flag = num
+    add = array.count(max_flag)
+    for i in range(add):
+        array.remove(max_flag)
+        sorted_array.insert(0,max_flag)
+    while len(array) != 0:
+        return selection_recursive(array)
+    print(sorted_array)
+selection_recursive([6, 5, 5, 7, 4, 8, 3, 5, 1, 2, 9])
