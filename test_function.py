f297b526d9565d4e2bdfcf0232d093df15297168
Khanh Linh Le <142861969+lklinz@users.noreply.github.com>
2024-03-28 22:41:13 +0000

    evolutionary_algorithms

--- test_function.py
+++ test_function.py
@@ -1,21 +1,21 @@
-tilings = []
+sub_set = []
 
-def generate_tilings(partial_tiling, length):
-    if sum(partial_tiling) == length:
-        global tilings
-        tilings.append(partial_tiling)
-    
-    elif sum(partial_tiling) > length:
-        return False
+def subset_sum(partial_sum, remaining_ints, target):
+    if sum(partial_sum) == target:
+        global sub_set
+        sub_set.append(partial_sum)
 
     else:
+        for int in remaining_ints:
 
-        for tile in range(1,length+1):
-
-            partial_tiling_copy = partial_tiling.copy()
-            partial_tiling_copy.append(tile)
-
-            generate_tilings(partial_tiling_copy,length)
-            
-generate_tilings([],4)
-print('Found tilings',tilings)
+            partial_sum_copy = partial_sum.copy()
+            if len(partial_sum_copy) == 0 or partial_sum_copy[-1] < int:
+                partial_sum_copy.append(int)
+                if sum(partial_sum_copy) <= target: 
+                
+                    remaining_ints_copy = remaining_ints.copy()
+                    remaining_ints_copy.remove(int)
+                    subset_sum(partial_sum_copy,remaining_ints_copy,target)
+        
+subset_sum([], [1, 2, 3, 4, 5, 6, 7], 6)
+print('Sub Set Sum',sub_set)
